add timestamp and fix yaw rotation

main_nerf.py
@@ -11,7 +11,7 @@ from loss import huber_loss
 #torch.autograd.set_detect_anomaly(True)
 
 if __name__ == '__main__':
-
+    print('[TIMESTAMP] start time:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str)
     parser.add_argument('-O', action='store_true', help="equals --fp16 --cuda_ray --preload")
@@ -34,9 +34,12 @@ if __name__ == '__main__':
     parser.add_argument('--patch_size', type=int, default=1, help="[experimental] render patches in training, so as to apply LPIPS loss. 1 means disabled, use [64, 32, 16] to enable")
 
     ### test options
-    parser.add_argument('--fps', type=int, default=25, help="test video fps")
-    parser.add_argument('--duration', type=int, default=5, help="video duration second")
-    parser.add_argument('--view', type=str, default='yaw', help="view direction:random pitch yaw roll")
+    parser.add_argument('--fps', type=int, default=15, help="test video fps")
+    parser.add_argument('--duration', type=int, default=10, help="video duration second")
+    parser.add_argument('--view', type=str, default='yaw', help="view direction:random or yaw")
+
+    ### evaluate options
+    parser.add_argument('--eval_interval', type=int, default=5, help="eval_interval")
 
     ### network backbone options
     parser.add_argument('--fp16', action='store_true', help="use amp mixed precision training")
@@ -125,25 +128,29 @@ if __name__ == '__main__':
         
         else:
             test_loader = NeRFDataset(opt, device=device, type='test').dataloader()
+            print('[TIMESTAMP] load data end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
             if test_loader.has_gt:
                 trainer.evaluate(test_loader) # blender has gt, so evaluate it.
-    
+                print('[TIMESTAMP] evaluate end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
             trainer.test(test_loader, write_video=True) # test and save video
-            
+            print('[TIMESTAMP] test end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
             trainer.save_mesh(resolution=256, threshold=10)
-    
+            print('[TIMESTAMP] save mesh end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     else:
 
         optimizer = lambda model: torch.optim.Adam(model.get_params(opt.lr), betas=(0.9, 0.99), eps=1e-15)
 
         train_loader = NeRFDataset(opt, device=device, type='train').dataloader()
+        print('[TIMESTAMP] load data end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
         # decay to 0.1 * init_lr at last iter step
         scheduler = lambda optimizer: optim.lr_scheduler.LambdaLR(optimizer, lambda iter: 0.1 ** min(iter / opt.iters, 1))
 
         metrics = [PSNRMeter(), LPIPSMeter(device=device)]
-        trainer = Trainer('ngp', opt, model, device=device, workspace=opt.workspace, optimizer=optimizer, criterion=criterion, ema_decay=0.95, fp16=opt.fp16, lr_scheduler=scheduler, scheduler_update_every_step=True, metrics=metrics, use_checkpoint=opt.ckpt, eval_interval=50)
+        trainer = Trainer('ngp', opt, model, device=device, workspace=opt.workspace, optimizer=optimizer, criterion=criterion, ema_decay=0.95, fp16=opt.fp16, lr_scheduler=scheduler, scheduler_update_every_step=True, metrics=metrics, use_checkpoint=opt.ckpt, eval_interval=opt.eval_interval)
 
         if opt.gui:
             gui = NeRFGUI(opt, trainer, train_loader)
@@ -157,13 +164,17 @@ if __name__ == '__main__':
                 max_epoch = opt.epochs
 
             trainer.train(train_loader, valid_loader, max_epoch)
+            print('[TIMESTAMP] trainning end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
             # also test
             test_loader = NeRFDataset(opt, device=device, type='test').dataloader()
             
             if test_loader.has_gt:
                 trainer.evaluate(test_loader) # blender has gt, so evaluate it.
-            
+                print('[TIMESTAMP] evaluate end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
             trainer.test(test_loader, write_video=True) # test and save video
-            
+            print('[TIMESTAMP] test end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
             trainer.save_mesh(resolution=256, threshold=10)
+            print('[TIMESTAMP] save mesh  end:', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))

nerf/provider.py
@@ -170,7 +170,6 @@ class NeRFDataset:
         
         # for colmap, manually interpolate a test set.
         if self.mode == 'colmap' and type == 'test':
-            
             # choose two random poses, and interpolate between.
             f0, f1 = np.random.choice(frames, 2, replace=False)
             pose0 = nerf_matrix_to_ngp(np.array(f0['transform_matrix'], dtype=np.float32), scale=self.scale, offset=self.offset) # [4, 4]
@@ -178,47 +177,26 @@ class NeRFDataset:
             rots = Rotation.from_matrix(np.stack([pose0[:3, :3], pose1[:3, :3]]))
             slerp = Slerp([0, 1], rots)
 
-            if self.view != 'random':
-                pose0 = nerf_matrix_to_ngp(np.array(frames[0]['transform_matrix'], dtype=np.float32), scale=self.scale, offset=self.offset)
-                pose0_rotation = Rotation.from_matrix(pose0[:3,:3])
-                pose0_xyz_angle = pose0_rotation.as_euler("xyz", degrees=True)
-                pose1_xyz_angle = []
-                pose2_xyz_angle = []
-                if self.view == 'yaw':
-                    pose1_xyz_angle = [pose0_xyz_angle[0]+180, pose0_xyz_angle[1], pose0_xyz_angle[2]]
-                    pose2_xyz_angle = [pose0_xyz_angle[0]-180, pose0_xyz_angle[1], pose0_xyz_angle[2]]
-                elif self.view == 'pitch':
-                    pose1_xyz_angle = [pose0_xyz_angle[0]+180, pose0_xyz_angle[1], pose0_xyz_angle[2]]
-                    pose2_xyz_angle = [pose0_xyz_angle[0]-180, pose0_xyz_angle[1], pose0_xyz_angle[2]]
-                elif self.view == 'roll':
-                    pose1_xyz_angle = [pose0_xyz_angle[0], pose0_xyz_angle[1]+180, pose0_xyz_angle[2]]
-                    pose2_xyz_angle = [pose0_xyz_angle[0], pose0_xyz_angle[1]-180, pose0_xyz_angle[2]]
-                pose1 = Rotation.from_euler('xyz', pose1_xyz_angle, degrees=True).as_matrix() # +180
-                pose2 = Rotation.from_euler('xyz', pose2_xyz_angle, degrees=True).as_matrix() # -180               
-                rots = Rotation.from_matrix(np.stack([pose0[:3, :3], pose1]))
-                slerp = Slerp([0, 1], rots)
-
             self.poses = []
             self.images = None
-            r = np.linalg.norm([pose0[1,3],pose0[2,3]])
-            for i in range(n_test + 1):
-                ratio = np.sin(((i / n_test) - 0.5) * np.pi) * 0.5 + 0.5
-                pose = np.eye(4, dtype=np.float32)
-                pose[:3, :3] = slerp(ratio).as_matrix()
-                print(pose)
-                print(pose0)
-                print(pose1)
-                if self.view == 'yaw':
-                    angel = (i/n_test)*2*np.pi
-                    #x = r*np.sin(angel)
-                    x = pose0[0, 3]
-                    #y = pose0[1,3]
-                    y = r*np.sin(angel)
-                    z = r*np.cos(angel)
-                    pose[:3,3] = np.array([x,y,z])
-                    pose[:3,3] = pose0[:3,3]
+            
+            pose_center = np.eye(4, dtype=np.float32)
+            pose_center[:3, 3] = np.array([0, 0, 0])
+            for i in range(0, 360, int(360/n_test)):
+                if self.view == 'random':
+                    ratio = np.sin(((i / n_test) - 0.5) * np.pi) * 0.5 + 0.5
+                    pose = np.eye(4, dtype=np.float32)
+                    pose[:3, :3] = slerp(ratio).as_matrix()
+                elif self.view == 'yaw':
+                    pose_center[:3, :3] = Rotation.from_euler('y', [i], degrees=True).as_matrix()
+                    pose = np.matmul(pose_center, pose0)
+                elif self.view == 'pitch':
+                    pose_center[:3, :3] = Rotation.from_euler('x', [i], degrees=True).as_matrix()
+                    pose = np.matmul(pose_center, pose0)
+                elif self.view == 'roll':
+                    pose_center[:3, :3] = Rotation.from_euler('z', [i], degrees=True).as_matrix()
+                    pose = np.matmul(pose_center, pose0)
                 self.poses.append(pose)
-
         else:
             # for colmap, manually split a valid set (the first frame).
             if self.mode == 'colmap':